44bec71c6a1c986c871e3c56e1dbdb7142dd5a8b
Sachin Shakya <sshakya219@gmail.com>
2023-05-09 11:14:24 +0530

    Source Code

---
+++ src/main/java/com/mikesh/Onito/Assessment/OnitoAssessmentApplication.java
@@ -0,0 +1,14 @@
+package com.mikesh.Onito.Assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnitoAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnitoAssessmentApplication.class, args);
+		System.out.println("Hey, it is working...");
+	}
+
+}
---
+++ src/test/java/com/mikesh/Onito/Assessment/OnitoAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikesh.Onito.Assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnitoAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
